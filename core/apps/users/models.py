2a2bf2f1ff11bc6b1416ce06dc668286cde2f5a5
Juan <jnbetancourt08@gmail.com>
2023-06-13 09:14:28 -0500

    use auth default 'user' schema

--- core/apps/users/models.py
+++ core/apps/users/models.py
@@ -1,15 +1,9 @@
-from django.db import models
-# Create your models here.
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-class Users(models.Model):
-    class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
-
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return f"Email: {self.email} - Name: {self.name}"
-    
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
