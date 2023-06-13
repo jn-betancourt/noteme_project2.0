794e5ba0e3fa900275855aa076e22e95aaf39358
Juan <jnbetancourt08@gmail.com>
2023-06-13 09:15:31 -0500

    api endpoints modified and added auth

--- core/apps/users/views.py
+++ core/apps/users/views.py
@@ -2,46 +2,20 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 # DJANGO HASHER
 import django.contrib.auth.hashers as hasher
+from django.contrib.auth.models import User
 
 # MODELS AND SERILIZERS
 from .serializers import UserCreationSerializer, UserRetrieveSerializer
-from .models import Users
 
 
-class UserManagement(APIView):
-    """API view for users actions such us:
-    - log in
-    - register
+class UserCreation(APIView):
+    """API view for User actions such us:
+    - register a new user
     """
-
-    def get(self, request, format=None):
-        """Handles GET request
-
-        Args:
-            request: First argument Django http base class provide.
-            format: Body format.
-        """
-
-        status_message = {"response": "user email or password wrong"}
-        status_http = status.HTTP_404_NOT_FOUND
-
-        user = UserRetrieveSerializer(request.data)  # info from QUERYDICT --> DICT
-        email = user.data.get("email")
-        password = user.data.get("password")
-
-        query = Users.objects.filter(email=email).first()  # lookup for the email
-        check_password = hasher.check_password(password, query.password)
-
-        if query and check_password:  # if email and password are correct
-            status_message = {
-                "response": {"id": query.id, "name": query.name, "email": query.email}
-            }
-            status_http = status.HTTP_200_OK
-
-        return Response(status_message, status_http)
 
     def post(self, request, format=None):
         """Handles POST request
@@ -50,32 +24,71 @@ class UserManagement(APIView):
             request: First argument Django http base class provide.
             format: Body format.
         """
-
         status_message = {"response": "user email or password wrong"}
         status_http = status.HTTP_406_NOT_ACCEPTABLE
-
         user_info = UserCreationSerializer(
             request.data
         )  # info from  QUERYDICT --> DICT
         email = user_info.data.get("email", None)  # Take the email
-        query = Users.objects.filter(email=email).first()  # Check if it is register
+        query = User.objects.filter(email=email).first()  # Check if it is register
 
         if not query:  # If not register --> register new user
             # Create a hash of a password for sec porpuses
             usable_password = hasher.make_password(user_info.data.get("password"))
-            new_user = Users.objects.create(
+
+            new_user = User.objects.create(
                 email=email,
-                name=user_info.data.get("name"),
+                username=user_info.data.get("username"),
                 password=usable_password,
             )
+
             new_user.save()
+            token, created = Token.objects.get_or_create(user=new_user)
             status_message = {
                 "response": {
-                    "id": Users.objects.filter(email=email).first().id,
-                    "name": user_info.data.get("name"),
+                    "token": token.key,
+                    "id": User.objects.filter(email=email).first().id,
+                    "name": user_info.data.get("username"),
                     "email": user_info.data.get("email"),
                 }
             }
-            status_http = status.HTTP_201_CREATED
+            status_http = status.HTTP_200_OK
+
+        return Response(status_message, status_http)
+
+
+class UserAuth(APIView):
+    """API view for User actions such us:
+    - Log In a user
+    """
+
+    def post(self, request, format=None):
+        """Handles POST request
+
+        Args:
+            request: First argument Django http base class provide.
+            format: Body format.
+        """
+        status_message = {"response": "user email or password wrong"}
+        status_http = status.HTTP_404_NOT_FOUND
+
+        user = UserRetrieveSerializer(request.data)  # info from QUERYDICT --> DICT
+        email = user.data.get("email")
+        password = user.data.get("password")
+
+        query = User.objects.get(email=email)  # lookup for the email
+        check_password = hasher.check_password(password, query.password)
+
+        if query and check_password:  # if email and password are correct
+            token = Token.objects.get(user=query)
+            status_message = {
+                "response": {
+                    "toke": token.key,
+                    "id": query.id,
+                    "username": query.username,
+                    "email": query.email,
+                }
+            }
+            status_http = status.HTTP_200_OK
 
         return Response(status_message, status_http)
