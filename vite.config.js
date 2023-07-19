e6361297b68cb77a2b62456202f8481cad286e26
Juan <jnbetancourt08@gmail.com>
2023-07-19 13:16:58 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react({
-        include: '**/*.{jsx,tsx,js}',
-    })],
-    server: {
-        port: 3000
-    }
+  plugins: [
+    react({
+      include: "**/*.{jsx,tsx,js}",
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+    assetsDir: "static",
+  },
 });
